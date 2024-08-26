Fixed eigenvalue/lambda calculation.
Moved the interfaced from m = kernel(f) to kernel(m,f) to allow
for in-place modificiation of the return arrays.

Also fixed the Bosse-Krieger model implementation.

mct/__util__.py
@@ -32,49 +32,41 @@ class model_base (object):
     def cache (self):
         if 'base' in dir(self): return False
         return True
-    def get_kernel (self, phi, i, t):
+    def get_kernel (self, m, phi, i, t):
         if not self.cache() or not '__m__' in dir(self):
-            self.__m__ = self.make_kernel(phi,i,t)
+            self.__m__ = self.make_kernel(m,phi,i,t)
         return self.__m__
-    def get_dm (self, phi, dphi):
+    def get_dm (self, m, phi, dphi):
         if not self.cache() or not '__dm__' in dir(self):
-            self.__dm__ = self.make_dm(phi,dphi)
+            self.__dm__ = self.make_dm(m,phi,dphi)
         return self.__dm__
-    def get_dmhat (self, f, ehat):
+    def get_dmhat (self, m, f, ehat):
         if not self.cache() or not '__dmhat__' in dir(self):
-            self.__dmhat__ = self.make_dmhat(f,ehat)
+            self.__dmhat__ = self.make_dmhat(m,f,ehat)
         return self.__dmhat__
-    def get_dm2 (self, phi, dphi):
+    def get_dm2 (self, m, phi, dphi):
         if not self.cache() or not '__dm2__' in dir(self):
-            self.__dm2__ = self.make_dm2(phi,dphi)
+            self.__dm2__ = self.make_dm2(m,phi,dphi)
         return self.__dm2__
 
     def make_kernel (self, phi, i, t):
-        M = len(self)
         @nb.njit
-        def dummy(phi, i, t):
-            return np.zeros(M)
+        def dummy(m, phi, i, t):
+            return
         return dummy
-    def make_dm (self, phi, dphi):
-        M = len(self)
+    def make_dm (self, m, phi, dphi):
         @nb.njit
-        def dummy(phi, dphi):
-            return np.zeros(M)
+        def dummy(m, phi, dphi):
+            return
         return dummy
-    def make_dmhat(self, f, ehat):
-        M = len(self)
+    def make_dmhat(self, m, f, ehat):
         @nb.njit
-        def dummy(f, ehat):
-            return np.zeros(M)
+        def dummy(m, f, ehat):
+            return
         return dummy
-    def make_dm2 (self, phi, dphi):
-        M = len(self)
+    def make_dm2 (self, m, phi, dphi):
         @nb.njit
-        def dummy(phi, dphi):
-            return np.zeros(M)
+        def dummy(m, phi, dphi):
+            return
         return dummy
 
-
-def solver_base(object):
-    def phi_addr(self):
-        return None

mct/fsolver.py
@@ -8,11 +8,11 @@ from .__util__ import void
 def _fsolve (f, m, kernel, M, accuracy, maxiter):
     iterations = 0
     converged = False
-    newf = np.ones((M,1))
+    newf = np.ones((1,M))
     while (not converged and iterations < maxiter):
         iterations+=1
         f[:] = newf
-        m[:] = kernel (f,0,0.)
+        kernel (m[0], f[0], 0, 0.)
         newf = m / (1.0+m)
         if np.isclose (newf, f, rtol=accuracy, atol=0.0).all():
             converged = True
@@ -24,13 +24,14 @@ class nonergodicity_parameter (object):
         self.model = model
         self.accuracy = accuracy
         self.maxiter = maxiter
-        self.f = np.zeros((len(model),1))
-        self.m = np.zeros((len(model),1))
-        self.jit_kernel = model.get_kernel(self.f,0,0.0)
+        self.f = np.zeros((1,len(model)))
+        self.m = np.zeros((1,len(model)))
+        self.jit_kernel = model.get_kernel(self.m,self.f,0,0.0)
         self.model.set_base(self.f)
 
     def solve (self):
         print (void(self.f))
+        print (self.f.shape)
         _fsolve(self.f, self.m, self.jit_kernel, len(self.model), self.accuracy, self.maxiter)
         print (void(self.f))
 
@@ -43,14 +44,14 @@ def _esolve(e, dm, f, M, maxiter, accuracy):
     while (not converged and iterations < maxiter):
         e[:] = newe
         #newe = (1-f)*model.dm(f,self.e)*(1-f)
-        newe = (1-f)*dm(f,e)*(1-f)
+        dm(newe,f,e)
         norm = np.sqrt(np.dot(newe,newe))
         if norm>1e-10: newe = newe/norm
         if np.isclose (newe, e, rtol=accuracy, atol=0.0).all():
             converged = True
             e[:] = newe
             eval_ = norm
-    return eval_,e
+    return eval_
 
 @njit
 def _ehatsolve(ehat, dmhat, f, M, maxiter, accuracy):
@@ -59,14 +60,14 @@ def _ehatsolve(ehat, dmhat, f, M, maxiter, accuracy):
     newehat = np.ones(M)
     while (not converged and iterations < maxiter):
         ehat[:] = newehat
-        newehat = dmhat(f,ehat)
+        dmhat(newehat,f,ehat)
         norm = np.sqrt(np.dot(newehat,newehat))
         if norm>1e-10: newehat = newehat/norm
         if np.isclose (newehat,ehat,rtol=accuracy,atol=0.0).all():
             converged = True
             ehat[:] = newehat
             eval2_ = norm
-    return eval2_,ehat
+    return eval2_
 
 class eigenvalue (object):
 
@@ -74,23 +75,27 @@ class eigenvalue (object):
         self.nep = nep
         self.accuracy = accuracy
         self.maxiter = maxiter
-        self.dm = self.nep.model.get_dm(self.nep.f,self.nep.f)
-        self.dmhat = self.nep.model.get_dmhat(self.nep.f,self.nep.f)
-        self.dm2 = self.nep.model.get_dm2(self.nep.f,self.nep.f)
+        self.dm = self.nep.model.get_dm(self.nep.m,self.nep.f,self.nep.f)
+        self.dmhat = self.nep.model.get_dmhat(self.nep.m,self.nep.f,self.nep.f)
+        self.dm2 = self.nep.model.get_dm2(self.nep.m,self.nep.f,self.nep.f)
 
     def solve (self):
-        f = self.nep.f
+        f = self.nep.f[0]
         model = self.nep.model
         self.e = np.zeros(len(model))
         self.ehat = np.zeros(len(model))
-        self.eval, self.e = _esolve(self.e, self.dm, f, len(model), self.maxiter, self.accuracy)
-        self.eval2, self.ehat = _ehatsolve(self.ehat, self.dm, f, len(model), self.maxiter, self.accuracy)
+        self.eval = _esolve(self.e, self.dm, f, len(model), self.maxiter, self.accuracy)
+        self.eval2 = _ehatsolve(self.ehat, self.dmhat, f, len(model), self.maxiter, self.accuracy)
         if self.eval > 0:
+            print("U",self.e,self.ehat)
             nl = np.dot(self.ehat, self.e)
             nr = np.dot(self.ehat, self.e*self.e / (1-f))
             self.e = self.e * nl/nr
             self.ehat = self.ehat * nr/(nl*nl)
-            self.lam = np.dot(self.ehat, (1-f)*self.dm2(f,self.e)*(1-f))
+            #self.lam = np.dot(self.ehat, (1-f)*self.dm2(f,self.e)*(1-f))
+            C = np.zeros(len(model))
+            self.dm2(C,f,self.e)
+            self.lam = np.dot(self.ehat, C)
             #nr = self.ehat * self.e*self.e / (1-f)
             #print ("nr {}".format(nr))
             #self.lam = self.lam / nr

mct/schematic.py
@@ -9,16 +9,12 @@ class schematic (model_base):
         self.M = M
     def __len__ (self):
         return self.M
-    def make_kernel (self, phi, i, t):
+    def make_kernel (self, m, phi, i, t):
         F = self.func
         @njit
-        def ker(phi, i, t):
-            return F(phi)
+        def ker(m, phi, i, t):
+            m = F(phi)
         return ker
-
-@njit
-def f12kernel(phi,v1,v2):
-    return v1*phi+v2*phi*phi
 
 class f12model (model_base):
     def __init__ (self, v1, v2):
@@ -26,32 +22,32 @@ class f12model (model_base):
         self.v2 = v2
     def __len__ (self):
         return 1
-    def make_kernel (self, phi, i, t):
+    def make_kernel (self, m, phi, i, t):
         v1 = self.v1
         v2 = self.v2
         @njit
-        def m(phi, i, t):
-            return  v1*phi + v2*phi*phi
-        return m
-    def make_dm (self, phi, dphi):
+        def ker(m, phi, i, t):
+            m[:] = v1*phi + v2*phi*phi
+        return ker
+    def make_dm (self, m, phi, dphi):
         v1 = self.v1
         v2 = self.v2
         @njit
-        def dm(phi, dphi):
-            return v1 * dphi + 2*v2 * phi*dphi
+        def dm(m, phi, dphi):
+            m[:] = (1-phi) * (v1 * dphi + 2*v2 * phi*dphi) * (1-phi)
         return dm
-    def make_dmhat (self, f, ehat):
+    def make_dmhat (self, m, f, ehat):
         v1 = self.v1
         v2 = self.v2
         @njit
-        def dmhat(f, ehat):
-            return (1-f)*(v1*ehat + 2*v2 * f*ehat)*(1-f)
+        def dmhat(m, f, ehat):
+            m[:] = (1-f)*(v1*ehat + 2*v2 * f*ehat)*(1-f)
         return dmhat
-    def make_dm2 (self, phi, dphi):
+    def make_dm2 (self, m, phi, dphi):
         v2 = self.v2
         @njit
-        def dm2(phi, dphi):
-            return v2 * dphi*dphi
+        def dm2(m, phi, dphi):
+            m[:] = (1-phi) * v2 * dphi*dphi * (1-phi)
         return dm2
 
 class f12gammadot_model (f12model):
@@ -59,15 +55,15 @@ class f12gammadot_model (f12model):
         f12model.__init__(self, v1, v2)
         self.gammadot = gammadot
         self.gammac = gammac
-    def make_kernel (self, phi, i, t):
+    def make_kernel (self, m, phi, i, t):
         v1 = self.v1
         v2 = self.v2
         gammadot = self.gammadot
         gammac = self.gammac
         @njit
-        def ker(phi, i, t):
+        def ker(m, phi, i, t):
             gt = gammadot*t / gammac
-            return (v1*phi + v2*phi*phi) * 1.0/(1.0 + gt*gt)
+            m[:] = (v1*phi + v2*phi*phi) * 1.0/(1.0 + gt*gt)
         return ker
 
 class sjoegren_model (model_base):
@@ -76,13 +72,13 @@ class sjoegren_model (model_base):
         self.base = base_model
     def __len__ (self):
         return 1
-    def make_kernel (self, phis, i, t):
+    def make_kernel (self, ms, phis, i, t):
         vs = self.vs
         base_phi = self.base.phi
         @njit
-        def ker(phis, i, t):
+        def ker(ms, phis, i, t):
             phi = nparray(base_phi)
-            return vs * phi[i] * phis
+            ms[:] = vs * phi[i] * phis
         return ker
 
 
@@ -93,13 +89,13 @@ class msd_model (model_base):
         self.base = base_model
     def __len__ (self):
         return 1
-    def make_kernel (self, phis, i, t):
+    def make_kernel (self, ms, phis, i, t):
         vs = self.vs
         base_phi = self.base.phi
         @njit
-        def ker(phis, i, t):
+        def ker(ms, phis, i, t):
             phi = nparray(base_phi)
-            return vs * phi[i] * phi[i]
+            ms[:] = vs * phi[i] * phi[i]
         return ker
 
 
@@ -110,19 +106,34 @@ class bosse_krieger_model (model_base):
         self.v3 = v3
     def __len__ (self):
         return 2
-    def m (self, phi=np.zeros(2), i=0, t=0):
-        return np.array([self.v1 * phi[0]*phi[0] + self.v2 * phi[1]*phi[1],
-                         self.v3 * phi[0]*phi[1]])
-    def dm (self, phi, dphi):
-        return np.array([2.*self.v1*phi[0]*dphi[0]+2.*self.v2*phi[1]*dphi[1],
-                         self.v3 * (phi[0]*dphi[1] + phi[1]*dphi[0])])
-    def dmhat (self, f, ehat):
-        return np.array([(1-f[0])*2.*self.v1*f[0]*ehat[0]*(1-f[0])
-                         +(1-f[1])*self.v3*f[1]*ehat[1]*(1-f[1]),
-                         (1-f[0])*2.*self.v2*f[1]*ehat[0]*(1-f[0])
-                         +(1-f[1])*self.v3*f[0]*ehat[1]*(1-f[1])])
-    def dm2 (self, phi, dphi):
-        return np.array([self.v1*dphi[0]*dphi[0]+self.v2*dphi[1]*dphi[1],
-                         self.v3 * dphi[0]*dphi[1]])
-
+    def make_kernel (self, m, phi, i, t):
+        v1, v2, v3 = self.v1, self.v2, self.v3
+        @njit
+        def ker (m, phi, i, t):
+            m[0] = v1 * phi[0]*phi[0] + v2 * phi[1]*phi[1]
+            m[1] = v3 * phi[0]*phi[1]
+        return ker
+    def make_dm (self, m, phi, dphi):
+        v1, v2, v3 = self.v1, self.v2, self.v3
+        @njit
+        def dm (m, phi, dphi):
+            m[0] = (1-phi[0]) * (2.*v1*phi[0]*dphi[0]+2.*v2*phi[1]*dphi[1]) * (1-phi[0])
+            m[1] = (1-phi[1]) * v3 * (phi[0]*dphi[1] + phi[1]*dphi[0]) * (1-phi[1])
+        return dm
+    def make_dmhat (self, m, f, ehat):
+        v1, v2, v3 = self.v1, self.v2, self.v3
+        @njit
+        def dmhat (m, f, ehat):
+            m[0] = ((1-f[0])*2.*v1*f[0]*ehat[0]*(1-f[0]) \
+                   +(1-f[1])*v3*f[1]*ehat[1]*(1-f[1]))
+            m[1] = ((1-f[0])*2.*v2*f[1]*ehat[0]*(1-f[0]) \
+                   +(1-f[1])*v3*f[0]*ehat[1]*(1-f[1]))
+        return dmhat
+    def make_dm2 (self, m, phi, dphi):
+        v1, v2, v3 = self.v1, self.v2, self.v3
+        @njit
+        def dm2 (m, phi, dphi):
+            m[0] = (1-phi[0])*(v1*dphi[0]*dphi[0]+v2*dphi[1]*dphi[1])*(1-phi[0])
+            m[1] = (1-phi[1])*v3 * dphi[0]*dphi[1] * (1-phi[1])
+        return dm2
 

mct/solver.py
@@ -45,7 +45,7 @@ def _solve_block (istart, iend, h, nutmp, phi, m, dPhi, dM, kernel, maxiter, acc
         newphi = phi[i-1]
         while (not converged and iterations < maxiter):
             phi[i] = newphi
-            m[i] = kernel (phi[i], i, h*i)
+            kernel (m[i], phi[i], i, h*i)
             newphi = B*m[i] - C
             iterations += 1
             if np.isclose (newphi, phi[i],
@@ -85,7 +85,7 @@ class correlator (object):
         self.accuracy = accuracy
 
         self.nu = nu
-        self.jit_kernel = model.get_kernel(self.phi_[0],0,0.0)
+        self.jit_kernel = model.get_kernel(self.m_[0],self.phi_[0],0,0.0)
         model.set_base(self.phi_)
 
     def initial_values (self, imax=50):
@@ -94,7 +94,7 @@ class correlator (object):
         for i in range(iend):
              t = i*self.h0
              self.phi_[i] = np.ones(self.mdimen) - t/self.nu
-             self.m_[i] = self.jit_kernel (self.phi_[i], i, t)
+             self.jit_kernel (self.m_[i], self.phi_[i], i, t)
         for i in range(1,iend):
              self.dPhi_[i] = 0.5 * (self.phi_[i-1] + self.phi_[i])
              self.dM_[i] = 0.5 * (self.m_[i-1] + self.m_[i])
@@ -149,7 +149,7 @@ def _msd_solve_block (istart, iend, h, nutmp, phi, m, dPhi, dM, kernel, maxiter,
         C += -6.0
         C = C/A
 
-        m[i] = kernel (None, i, h*i)
+        kernel (m[i], None, i, h*i)
         phi[i] = - C
 
 
@@ -161,7 +161,7 @@ class mean_squared_displacement (correlator):
         for i in range(iend):
             t = i*self.h0
             self.phi_[i] = np.ones(self.mdimen)*6.*t/self.nu
-            self.m_[i] = self.jit_kernel (None, i, t)
+            self.jit_kernel (self.m_[i], None, i, t)
         for i in range(1,iend):
             self.dPhi_[i] = 0.5 * (self.phi_[i-1] + self.phi_[i])
             self.dM_[i] = 0.5 * (self.m_[i-1] + self.m_[i])