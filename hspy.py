8b5ab9a1a69a85c203808775b383aec5c895f641
Thomas Voigtmann <thomas.voigtmann@dlr.de>
2024-09-07 11:38:08 +0200

    Allow multiple packing fractions.

--- hspy.py
+++ hspy.py
@@ -6,7 +6,7 @@ import mct
 
 parser = argparse.ArgumentParser()
 parser.add_argument ('-phi',metavar='phi',help='packing fraction',
-                     type=float, default=0.5)
+                     type=float, nargs='+', default=0.5)
 args = parser.parse_args()
 
 class hssPY (object):
@@ -44,81 +44,98 @@ class hssPY (object):
         # bigger than low-q
         return 1.0 / (1.0 - self.phi*6/np.pi * cq_), cq_
 
-sq = hssPY(phi=args.phi)
-
-qgrid = np.linspace(0.2,39.8,100)
-
-plt.plot(qgrid, sq.Sq(qgrid)[0])
-plt.show()
-
-model = mct.simple_liquid_model (sq, qgrid)
-phi = mct.correlator (model = model, store = True, blocks=50)
-correlators = mct.CorrelatorStack([phi])
-
-model_s = mct.tagged_particle_model (model, cs=sq)
-phi_s = mct.correlator (model = model_s, base=phi, store = True)
-correlators.append(phi_s)
-
-model_s0 = mct.tagged_particle_q0 (model_s)
-msd = mct.mean_squared_displacement (model = model_s0, base=phi_s, store=True)
-correlators.append(msd)
-
-if False:
-    f = mct.nonergodicity_parameter (model = model)
-    f.solve()
-    plt.plot(qgrid, f.f[0])
-    plt.show()
-    
-    ev = mct.eigenvalue (f)
-    ev.solve()
-    print ("eigenvalue",ev.eval,ev.eval2)
-    print ("lambda",ev.lam)
-    plt.plot(qgrid,ev.e*(1-f.f[0])**2)
-    plt.plot(qgrid,ev.ehat)
-    plt.show()
-
 def output (d, istart, iend, correlator_array):
     print ("block",d,"\r",end='')
 
-correlators.solve_all(callback=output)
-print("")
 
-# save:
-#phi.save("/tmp/phi.h5")
+qgrid = np.linspace(0.2,39.8,100)
 
-# restore:
-#phi2 = mct.correlator.load('/tmp/phi.h5')
-#correlators2 = mct.CorrelatorStack([phi2])
-#model_s2 = mct.tagged_particle_model (phi2.model, cs=sq)
-#phi_s2 = mct.correlator (model = model_s2, base=phi2, store = True)
-#correlators2.append(phi_s2)
-#correlators2.solve_all(callback=output)
-#print("")
+
+sqlist = []
+for packing_fraction in args.phi:
+    sq = hssPY(phi=packing_fraction)
+    plt.plot(qgrid, sq.Sq(qgrid)[0])
+    sqlist.append(sq)
+plt.show()
+
+phi_list = []
+phi_s_list = []
+msd_list = []
+ngp_list = []
+for packing_fraction,sq in zip(args.phi,sqlist):
+    # base model "simple liquid"
+    model = mct.simple_liquid_model (sq, qgrid)
+    phi = mct.correlator (model = model, store = True, blocks=50)
+    correlators = mct.CorrelatorStack([phi])
+
+    # tagged-particle model
+    model_s = mct.tagged_particle_model (model, cs=sq)
+    phi_s = mct.correlator (model = model_s, base=phi, store = True)
+    correlators.append(phi_s)
+
+    # q=0 limit of tagged particle to determine the MSD
+    model_s0 = mct.tagged_particle_q0 (model_s)
+    msd = mct.mean_squared_displacement (model = model_s0, base=phi_s, store=True)
+    correlators.append(msd)
+
+    if False:
+        f = mct.nonergodicity_parameter (model = model)
+        f.solve()
+        plt.plot(qgrid, f.f[0])
+        plt.show()
+    
+        ev = mct.eigenvalue (f)
+        ev.solve()
+        print ("eigenvalue",ev.eval,ev.eval2)
+        print ("lambda",ev.lam)
+        plt.plot(qgrid,ev.e*(1-f.f[0])**2)
+        plt.plot(qgrid,ev.ehat)
+        plt.show()
+
+
+    print("packing fraction",packing_fraction)
+    correlators.solve_all(callback=output)
+    print("")
+
+    # save:
+    #phi.save("/tmp/phi.h5")
+
+    # restore:
+    #phi2 = mct.correlator.load('/tmp/phi.h5')
+    #correlators2 = mct.CorrelatorStack([phi2])
+    #model_s2 = mct.tagged_particle_model (phi2.model, cs=sq)
+    #phi_s2 = mct.correlator (model = model_s2, base=phi2, store = True)
+    #correlators2.append(phi_s2)
+    #correlators2.solve_all(callback=output)
+    #print("")
+
+    phi_list.append(phi)
+    phi_s_list.append(phi_s)
+    msd_list.append(msd)
 
 
 qval = 7.4
 qi = np.nonzero(np.isclose(qgrid,qval))[0][0]
-print(phi.phi.shape)
-plt.plot(phi.t, phi.phi[:,qi], label='phi')
-plt.plot(phi_s.t, phi_s.phi[:,qi], label='phi_s')
-plt.plot(phi.t, np.exp(-qval**2*phi.t),color='black',linestyle='dashed')
-#plt.plot(phi.t, phi.m[:,qi])
+for packing_fraction,phi,phi_s in zip(args.phi,phi_list,phi_s_list):
+    plt.plot(phi.t, phi.phi[:,qi], label='phi(t), phi={}'.format(packing_fraction))
+    plt.plot(phi_s.t, phi_s.phi[:,qi], label='phi_s(t), phi={}'.format(packing_fraction))
+    plt.plot(phi.t, np.exp(-qval**2*phi.t),color='black',linestyle='dashed')
 plt.xscale('log')
 plt.legend()
 plt.show()
 
-quit()
-
-for corr in [phi,phi_s]:
-    tau_indices = corr.phi.shape[0] - np.sum(corr.phi<=0.1,axis=0)
-    tau_indices[tau_indices>=corr.phi.shape[0]] = -1
-    tau = corr.t[tau_indices]
-    plt.plot(model.q,tau)
+for packing_fraction,phi,phi_s in zip(args.phi,phi_list,phi_s_list):
+    for corr in [phi,phi_s]:
+        tau_indices = corr.phi.shape[0] - np.sum(corr.phi<=0.1,axis=0)
+        tau_indices[tau_indices>=corr.phi.shape[0]] = -1
+        tau = corr.t[tau_indices]
+        plt.plot(model.q,tau)
 plt.yscale('log')
 plt.show()
 
-plt.plot(msd.t, msd.phi[:,0])
-plt.plot(msd.t, 6*msd.t, color='black', linestyle='dashed')
+for packing_fraction,msd in zip(args.phi,msd_list):
+    plt.plot(msd.t, msd.phi[:,0])
+    plt.plot(msd.t, 6*msd.t, color='black', linestyle='dashed')
 plt.xscale('log')
 plt.yscale('log')
 plt.show()
