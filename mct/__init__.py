9de5bf320af8ed688f11313b0781e4f72107ebc2
Thomas Voigtmann <thomas.voigtmann@dlr.de>
2024-09-08 14:39:20 +0200

    Added Filon integration routine.
    Useful for calculating spectra of correlation functions.

--- mct/__init__.py
+++ mct/__init__.py
@@ -2,6 +2,6 @@ from .solver import correlator, mean_squared_displacement, non_gaussian_paramete
 from .fsolver import nonergodicity_parameter, eigenvalue
 from .schematic import f12model,f12gammadot_model,sjoegren_model,bosse_krieger_model
 from .standard import simple_liquid_model, tagged_particle_model, tagged_particle_q0, tagged_particle_ngp
-from .util import CorrelatorStack
+from .util import CorrelatorStack, filon_integrate, filon_cos_transform, filon_sin_transform
 
 from .__util__ import model_base
--- mct/util.py
+++ mct/util.py
@@ -17,3 +17,57 @@ class CorrelatorStack(list):
             for _phi_ in self:
                 _phi_.decimize ()
 
+
+def filon_integrate(f,x,G0,G1):
+    r"""Filon-Tuck integration routine for weighted integrals.
+
+    Integrates the function :math:`f(x)` given on a fixed grid of
+    points :math:`x` weighted with a function :math:`g(x)` for which the
+    integral as well as the integral over :math:`xg(x)` are known
+    analytically.
+
+    Parameters
+    ----------
+    f : array_like
+        Function values on the grid `x`
+    x : array_like
+	Points corresponding to the function values `f`.
+    G0 : callable
+        Analytic implementation of the integral over the weight function.
+    G1 : callable
+        Analytic implementation of the integral over x times weight function.
+
+    Notes
+    -----
+    The integration is performed as
+    .. math::
+
+        \int_{x_i}^{x_{i+1}}f(x)g(x)\,dx =
+        a \int_{x_i}^{x_{i+1}} g(x)\,dx + b \int_{x_i}^{x_{i+1}} xg(x)\,dx
+
+    and it is assumed that the integrals over :math:`g(x)` and
+    :math:`xg(x)` are known analytically and provided as arguments to
+    this function.
+
+    Setting G0=lambda x:x and G1=lambda x:x*x/2 evaluates the integral
+    over f following the ordinary trapezoidal rule.
+
+    Setting G0=lambda x:np.sin(x) and G1=lambda x:x*np.sin(x)+np.cos(x)
+    evaluates the Fourier-cosine integral over f following the method
+    suggested by Tuck.
+    """
+    b = np.diff(f,axis=-1)/np.diff(x,axis=-1)
+    a = f[...,:-1] - x[...,:-1]*b
+    return np.sum(a*np.diff(G0(x)) + b*np.diff(G1(x)),axis=-1)
+
+
+# for these, f,x,w need to have shape (M,N) where N is the number of
+# time-domain data points, and M the number of frequency data points
+# ie use x_,w_ = np.meshgrid(x,w)
+# f_,_ = np.meshgrid(f,w)
+def filon_cos_transform(f,x,w):
+    return filon_integrate(f,x,lambda x:np.sin(w*x)/w,
+                               lambda x:x*np.sin(w*x)/w+np.cos(w*x)/w**2)
+def filon_sin_transform(f,x,w):
+    return filon_integrate(f,x,lambda x:-np.cos(w*x)/w,
+                               lambda x:-x*np.cos(w*x)/w+np.sin(w*x)/w**2)
